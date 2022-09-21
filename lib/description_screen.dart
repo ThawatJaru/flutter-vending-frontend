f9db82d42156bf2a0758872b14eaaa358ca37c02
Thawat Jarupenpat <goldenguyz17@gmail.com>
2022-09-21 14:02:33 +0700

    initial

--- lib/description_screen.dart
+++ lib/description_screen.dart
@@ -1,3 +1,4 @@
+import 'package:automated_ios/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class DescriptionPage extends StatefulWidget {
@@ -13,16 +14,43 @@ class _DescriptionPage extends State<DescriptionPage> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        toolbarHeight: 50.0,
+        leading: IconButton(
+          icon:
+              const Icon(Icons.arrow_back_ios, color: Colors.black, size: 20.0),
+          onPressed: () => Navigator.of(context).pop(
+            const HomeScreen(),
+          ),
+        ),
+        // backgroundColor: Colors.green,
       ),
       body: Center(
-        child: Column(children: <Widget>[
-          Container(
-            color: Colors.red,
-            width: size.width * 0.3,
-            height: size.height * 0.2,
-          ),
-        ]),
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.only(top: 26.0),
+            ),
+            //Name Plant description page
+            const Text(
+              'Round Cactus',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            //Picture plant in description page
+            Container(
+              color: Colors.red,
+              width: size.width * 0.5,
+              height: size.height * 0.36,
+            ),
+          ],
+        ),
       ),
     );
   }
