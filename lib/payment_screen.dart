46b449542a09e3675d9597867e986bd5dfcaffda
Thawat Jarupenpat <goldenguyz17@gmail.com>
2022-12-09 16:22:21 +0700

    update mqtt subscribe

--- lib/payment_screen.dart
+++ lib/payment_screen.dart
@@ -2,16 +2,110 @@ import 'package:automated_ios/home_screen.dart';
 import 'package:automated_ios/plant_statement.dart';
 import 'package:flutter/material.dart';
 import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+
+String subscribed_message = "";
 
 class PaymentPage extends StatefulWidget {
   final PlantStatement statement;
   PaymentPage({super.key, required this.statement}) {
     print(statement.statement_id);
     print(statement.status);
+    MqttConnection();
   }
 
   @override
   State<PaymentPage> createState() => _PaymentPage();
+
+  //////////////////////////////////////////
+
+  Future MqttConnection() async {
+    MqttServerClient client = MqttServerClient.withPort(
+        "projecttech.thddns.net", "flutter-client", 5050);
+    //client.logging(on: true);
+
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    client.onUnsubscribed = onUnsubscribed;
+    client.onSubscribed = onSubscribed;
+    client.onSubscribeFail = onSubscribeFail;
+    client.pongCallback = pong;
+    client.keepAlivePeriod = 60;
+    client.setProtocolV311();
+
+    final connMessage = MqttConnectMessage()
+        .withWillTopic('willtopic')
+        .withWillMessage('Will message')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+
+    print('MQTT_LOGS::Mosquitto client connecting....');
+
+    // Try to Connect to broker
+    try {
+      await client.connect();
+      //client.onConnected;
+      //print("Connected");
+    } catch (e) {
+      print('Exception: $e');
+      client.disconnect();
+    }
+
+    if (client.connectionStatus!.state == MqttConnectionState.connected) {
+      print('MQTT_LOGS::Mosquitto client connected');
+    } else {
+      print(
+          'MQTT_LOGS::ERROR Mosquitto client connection failed - disconnecting, status is ${client.connectionStatus}');
+      client.disconnect();
+    }
+
+    subscribeTopic(client);
+  }
+
+  void subscribeTopic(MqttServerClient client) {
+    print('MQTT_LOGS::Subscribing to the test topic');
+    const topic = 'test';
+    client.subscribe(topic, MqttQos.atMostOnce);
+
+    client.updates!.listen((List<MqttReceivedMessage<MqttMessage?>>? c) {
+      final recMess = c![0].payload as MqttPublishMessage;
+      final pt =
+          MqttPublishPayload.bytesToStringAsString(recMess.payload.message);
+
+      subscribed_message = pt;
+      print(
+          'MQTT_LOGS:: New data arrived: topic is <${c[0].topic}>, payload is $pt');
+      print('');
+      client.disconnect();
+    });
+  }
+
+  /////////////////////////////
+  ////// Callback Preset //////
+
+  void onConnected() {
+    print('MQTT_LOGS:: Connected');
+  }
+
+  void onDisconnected() {
+    print('MQTT_LOGS:: Disconnected');
+  }
+
+  void onSubscribed(String topic) {
+    print('MQTT_LOGS:: Subscribed topic: $topic');
+  }
+
+  void onSubscribeFail(String topic) {
+    print('MQTT_LOGS:: Failed to subscribe $topic');
+  }
+
+  void onUnsubscribed(String? topic) {
+    print('MQTT_LOGS:: Unsubscribed topic: $topic');
+  }
+
+  void pong() {
+    print('MQTT_LOGS:: Ping response client callback invoked');
+  }
 }
 
 //class description
